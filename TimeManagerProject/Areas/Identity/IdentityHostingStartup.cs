8397bfffa3da2abc2d797be693f53cf87f3c91f2
valboaten@gmail.com <valboaten@gmail.com>
2021-03-20 14:24:41 -0500

    Comment out as conflicts with other Startup File in project.

--- TimeManagerProject/Areas/Identity/IdentityHostingStartup.cs
+++ TimeManagerProject/Areas/Identity/IdentityHostingStartup.cs
@@ -1,27 +1,27 @@
-using System;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using TimeManagerProject.Areas.Identity.Data;
+//using System;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.UI;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using TimeManagerProject.Areas.Identity.Data;
 
-[assembly: HostingStartup(typeof(TimeManagerProject.Areas.Identity.IdentityHostingStartup))]
-namespace TimeManagerProject.Areas.Identity
-{
-    public class IdentityHostingStartup : IHostingStartup
-    {
-        public void Configure(IWebHostBuilder builder)
-        {
-            builder.ConfigureServices((context, services) => {
-                services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseMySql(
-                        context.Configuration.GetConnectionString("MySQLConnection")));
+//[assembly: HostingStartup(typeof(TimeManagerProject.Areas.Identity.IdentityHostingStartup))]
+//namespace TimeManagerProject.Areas.Identity
+//{
+//    public class IdentityHostingStartup : IHostingStartup
+//    {
+//        public void Configure(IWebHostBuilder builder)
+//        {
+//            builder.ConfigureServices((context, services) => {
+//                services.AddDbContext<ApplicationDbContext>(options =>
+//                    options.UseMySql(
+//                        context.Configuration.GetConnectionString("MySQLConnection")));
 
-                //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                    //.AddEntityFrameworkStores<ApplicationDbContext>();
-            });
-        }
-    }
-}
+//                //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//                    //.AddEntityFrameworkStores<ApplicationDbContext>();
+//            });
+//        }
+//    }
+//}
