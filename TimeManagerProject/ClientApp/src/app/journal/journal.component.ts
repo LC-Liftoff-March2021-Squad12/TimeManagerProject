41f4dc184af4490b21aec1b1a99be49a1a4858e6
cbowser36 <72530740+cbowser36@users.noreply.github.com>
2021-04-14 19:24:01 -0500

    journal entry changes

--- TimeManagerProject/ClientApp/src/app/journal/journal.component.ts
+++ TimeManagerProject/ClientApp/src/app/journal/journal.component.ts
@@ -1,26 +1,26 @@
 import { Component } from '@angular/core';
 
 
+
 @Component({
   selector: 'journal-home',
   templateUrl: './journal.component.html',
   styleUrls: ['./journal.component.css']
 })
-
- 
- 
-
 export class JournalComponent {
-title = '';
-date = '';
-journal = '';
-entries=[];
 
-  getValues(){
-    this.title = document.getElementById('title').value + ' ';
-    this.date = document.getElementById('date').value;
-    this.journal = document.getElementById('journal').value;
+title: string;
+date: string;
+journal:string;
+entry: string
+ 
+getValues(){
+  this.journal = document.getElementById('journal');
+  this.date = document.getElementById('date');
+  this.title = document.getElementById('title');
   }
 
 
 }
+
+
