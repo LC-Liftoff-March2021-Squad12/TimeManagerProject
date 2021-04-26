9f9d2830e1de47feb2d9a70b350ce8a20506b47b
Gloria <hklover@gmail.com>
2021-04-26 17:38:13 -0500

    some changes

--- TimeManagerProject/ClientApp/src/app/task-service.service.ts
+++ TimeManagerProject/ClientApp/src/app/task-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Of } from 'rxjs-compat/observable/of';
+//import { of } from 'rxjs/observable/of';
 
 @Injectable({
   providedIn: 'root'
