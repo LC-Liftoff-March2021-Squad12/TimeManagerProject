602c88a145fc3cf71010496fa0a84eba9e140fc7
cbowser36 <72530740+cbowser36@users.noreply.github.com>
2021-04-26 14:37:47 -0500

    saving journal entries

--- TimeManagerProject/ClientApp/src/app/journal/journal.component.ts
+++ TimeManagerProject/ClientApp/src/app/journal/journal.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
+
 @Component({
   selector: 'journal-home',
   templateUrl: './journal.component.html',
@@ -9,37 +10,41 @@ import { HttpClient } from '@angular/common/http';
 })
 export class JournalComponent {
   public details: journalEntry[];
-private entry: journalEntry;
+public entry: [journalEntry];
 private title:string;
 private date: string;
 private body:string;
-private entries: journalEntry; 
+
 
 constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
-  ///http.get<journalEntry[]>(baseUrl + 'journalEntry').subscribe(result => {
-   // this.details = result;
- // }, error => console.error(error));
 }
 
-getValues(){
+saveValues(){
   this.body = (<HTMLInputElement>document.getElementById('body')).value;
   this.title = (<HTMLInputElement>document.getElementById('title')).value;
   this.date = (<HTMLInputElement>document.getElementById('date')).value;
 
-//  console.log(this.journal + this.title + this.date);
-  this.entry = { title: this.title, body: this.body, date: this.date };
+  this.entry = [{ title: this.title, body: this.body, date: this.date }];
 
-  this.http.post<any>(this.baseUrl + 'journalEntry',this.entry).subscribe(result => {
+  this.http.post<any>(this.baseUrl + 'journalEntry', this.entry).subscribe(result => {
     this.entry = result;
   }, error => console.error(error));
 }
+
+getValues(){
+  this.http.get<any>(this.baseUrl + 'journalEntry').subscribe(result => {
+      this.entry = result;
+    
+    
+  }, error => console.error(error));
+ 
+}
+
 }
 
 interface journalEntry {
   title: string;
   body: string;
   date: string;
-  // id: number; 
 }
-
 
