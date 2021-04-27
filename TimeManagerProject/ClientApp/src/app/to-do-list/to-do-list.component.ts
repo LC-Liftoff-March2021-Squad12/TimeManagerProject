b04da6b86e73769aed43505bbd8c93bb78113ee6
Gloria <hklover@gmail.com>
2021-04-27 13:53:44 -0500

    fixed css and added deleteTask function is ts file

--- TimeManagerProject/ClientApp/src/app/to-do-list/to-do-list.component.ts
+++ TimeManagerProject/ClientApp/src/app/to-do-list/to-do-list.component.ts
@@ -7,48 +7,47 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./to-do-list.component.css']
 })
 export class ToDoListComponent {
-  public toDoList: TaskList[];
-  public task: [TaskList];
+  public toDoList: TaskEntry[];
+  public task: [TaskEntry];
   private description: string;
   private isDone: boolean;
 
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
   }
 
+  getTask() {
+    this.http.get<any>(this.baseUrl + 'TaskEntry').subscribe(result => {
+      this.toDoList = result;
+    }, error => console.error(error));
+  }
+
   saveTask() {
-    this.toDoList.push(new TaskList);
+    //this.toDoList.push(new this.TaskEntry);
     this.description = (<HTMLInputElement>document.getElementById('description')).value;
 
     this.task = [{ description: this.description, isDone: this.isDone }];
 
-    this.http.post<any>(this.baseUrl + 'to-do-list', this.task).subscribe(result => {
+    this.http.post<any>(this.baseUrl + 'TaskEntry', this.task).subscribe(result => {
       this.task = result;
     }, error => console.error(error));
   }
 
-  getTask() {
-    this.http.get<any>(this.baseUrl + 'to-do-list').subscribe(result => {
-      this.task = result;
-    }, error => console.error(error));
-  }
-  
   //addTask() {
     //this.toDoList.push(new TaskList());
   //}
 
   deleteTask(index: number) {
-    this.http.delete<any>(this.baseUrl + 'to-do-list').subscribe(result => {
-      this.task = result;
-    }, error => console.error(error));
-
     if (index > -1) {
       this.toDoList.splice(index, 1);
     }
+    this.http.delete<any>(this.baseUrl + 'TaskEntry').subscribe(result => {
+      this.task = result;
+    }, error => console.error(error));
   }
 
 }
 
-interface TaskList {
+interface TaskEntry {
   description: string;
   isDone: boolean;
 }
