1ab77f6585b47589a0414d29f98325c5f8c51638
Gloria <hklover@gmail.com>
2021-04-27 12:54:35 -0500

    rewrote todolist html and css files

--- TimeManagerProject/ClientApp/src/app/to-do-list/to-do-list.component.ts
+++ TimeManagerProject/ClientApp/src/app/to-do-list/to-do-list.component.ts
@@ -1,23 +1,41 @@
-import { Task } from '../to-do-list/task.js';
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-to-do-list-component',
   templateUrl: './to-do-list.component.html',
   styleUrls: ['./to-do-list.component.css']
 })
-export class ToDoListComponent implements OnInit{
+export class ToDoListComponent {
+  public toDoList: TaskList[];
+  public task: [TaskList];
+  private description: string;
+  private isDone: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
   }
 
-  public toDoList: Task[] = [];
+  saveTask() {
+    this.description = (<HTMLInputElement>document.getElementById('description')).value;
 
-  addTask() {
-    this.toDoList.push(new Task());
+    this.task = [{ description: this.description, isDone: this.isDone }];
+
+    this.http.post<any>(this.baseUrl + 'to-do-list', this.task).subscribe(result => {
+      this.task = result;
+    }, error => console.error(error));
   }
+
+  getTask() {
+    this.http.get<any>(this.baseUrl + 'to-do-list').subscribe(result => {
+      this.task = result;
+
+
+    }, error => console.error(error));
+  }
+  
+  //addTask() {
+    //this.toDoList.push(new TaskList());
+  //}
 
   deleteTask(index: number) {
     if (index > -1) {
@@ -25,5 +43,10 @@ export class ToDoListComponent implements OnInit{
     }
   
   }
-
 }
+
+interface TaskList {
+  description: string;
+  isDone: boolean;
+}
+
